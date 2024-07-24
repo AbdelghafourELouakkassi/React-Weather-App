a5adde2d8daa21a8a2f26a172760fd947bdc1520
AbdelghafourELouakkassi <125968638+AbdelghafourELouakkassi@users.noreply.github.com>
2024-07-24 01:53:19 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ function App() {
 
   const getWeatherData=async(city)=>{
     try {
-      const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_WEATHER_API_KEY}`
+      const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_WEATHER_API}`
       const response=await fetch(url)
       const data=await response.json();
       if(!response.ok){
